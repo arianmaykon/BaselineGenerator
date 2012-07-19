3ab5fc5d8fd29a74a5547fa9ca13286537e23e9c
arianmaykon <arian.maykon@gmail.com>
2012-07-19 16:36:26 -0300

    Changing the user class for the vjGuard plugin.

--- apps/frontend/lib/myUser.class.php
+++ apps/frontend/lib/myUser.class.php
@@ -1,28 +1,5 @@
 <?php
 
-class myUser extends sfGuardSecurityUser
-{
-	/**
-	 *
-	 *
-	 */
-#	function checkLDAPPassword($username, $password) {
-#	public function checkPassword($username, $password) {
-	public function checkPassword($password) {
-#		$user = LDAP::getUser($username);
-#		if ($user->checkPassword($password)) {			
-#			return true;
-#		} else {
-#			return false;
-#		}
-#		return $this->authenticateOnLdap($username, $password);
-	}
+class myUser extends vjGuardADSecurityUser {
 
-	/**
-	 * @param string $username
-	 * @param string $password
-	 */
-	// private function authenticateOnLdap($username, $password) {
- //        return Util::getLdap()->authenticate(strtolower($username), $password);
- //    }
 }
