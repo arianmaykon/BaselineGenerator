6616379c1c3a8e50701c74a4fb46e61914e0459e
arianmaykon <arian.maykon@gmail.com>
2012-07-19 16:40:44 -0300

    Updating filters.

---
+++ lib/filter/doctrine/BaselineFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Baseline filter form.
+ *
+ * @package    baselinegenerator
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class BaselineFormFilter extends BaseBaselineFormFilter
+{
+  public function configure()
+  {
+  }
+}
---
+++ lib/filter/doctrine/ParameterFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Parameter filter form.
+ *
+ * @package    baselinegenerator
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class ParameterFormFilter extends BaseParameterFormFilter
+{
+  public function configure()
+  {
+  }
+}
